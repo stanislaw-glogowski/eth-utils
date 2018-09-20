ae5fe1714405006a4dd4894385b11903a6c21d6e
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2018-09-20 12:05:52 +0200

    Adds ecdsa utils

--- src/ecdsa/utils.ts
+++ src/ecdsa/utils.ts
@@ -1,5 +1,43 @@
 import { randomBytes } from "crypto";
-import { privateKeyVerify } from "secp256k1";
+import { publicKeyVerify, privateKeyVerify, publicKeyCreate } from "secp256k1";
+
+/**
+ * verifies public key
+ * @param publicKey
+ */
+export function verifyPublicKey(publicKey: Buffer): boolean {
+  return (
+    publicKey &&
+    Buffer.isBuffer(publicKey) &&
+    publicKeyVerify(publicKey)
+  );
+}
+
+/**
+ * verifies private key
+ * @param privateKey
+ */
+export function verifyPrivateKey(privateKey: Buffer): boolean {
+  return (
+    privateKey &&
+    Buffer.isBuffer(privateKey) &&
+    privateKeyVerify(privateKey)
+  );
+}
+
+/**
+ * converts private To public key
+ * @param privateKey
+ */
+export function privateToPublicKey(privateKey: Buffer): Buffer {
+  let result: Buffer = null;
+
+  if (verifyPrivateKey(privateKey)) {
+    result = publicKeyCreate(privateKey, false);
+  }
+
+  return result;
+}
 
 /**
  * generates random private key
@@ -8,7 +46,7 @@ export function generateRandomPrivateKey(): Buffer {
   let result: Buffer;
   for (; ;) {
     result = randomBytes(32) as Buffer;
-    if (privateKeyVerify(result)) {
+    if (verifyPrivateKey(result)) {
       break;
     }
   }
